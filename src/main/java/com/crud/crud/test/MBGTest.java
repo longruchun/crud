842f6b2d2a81c21761fc6f2fe2ebe5fff77d095b
似水流年 <1430468717@qq.com>
2018-12-06 17:04:33 +0800

    1

--- src/main/java/com/crud/crud/test/MBGTest.java
+++ src/main/java/com/crud/crud/test/MBGTest.java
@@ -6,8 +6,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
@@ -15,20 +13,19 @@ import org.mybatis.generator.exception.InvalidConfigurationException;
 import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
-
-
 public class MBGTest {
 
-	public static void main(String[] args) throws IOException, XMLParserException, SQLException, InterruptedException, InvalidConfigurationException {
+	public static void main(String[] args)
+			throws IOException, XMLParserException, SQLException, InterruptedException, InvalidConfigurationException {
 		// TODO Auto-generated method stub
 		List<String> warnings = new ArrayList<String>();
-		  boolean overwrite = true;
-		   File configFile = new File("mbg.xml");
-		   ConfigurationParser cp = new ConfigurationParser(warnings);
-		   Configuration config = cp.parseConfiguration(configFile);
-		   DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-		   MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-		   myBatisGenerator.generate(null);
+		boolean overwrite = true;
+		File configFile = new File("mbg.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		myBatisGenerator.generate(null);
 	}
 
 }
